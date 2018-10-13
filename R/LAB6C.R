a79912e565e17b11917390c9aa21c9d9e5ea522e
Zijie Feng <fengye0907@outlook.com>
2018-10-14 01:07:47 +0200

    Initial commit

---
+++ R/LAB6C.R
@@ -0,0 +1,12 @@
+#' LAB6C
+#'
+#' This is package for LAB6.
+#'
+#' @docType package
+#' @author Zijie Feng
+#' @author Andreas C.charitos
+#' @author Muhammad Daud
+#'
+#'
+#' @name LAB6C
+NULL
---
+++ R/knapsack.R
@@ -0,0 +1,112 @@
+#'knapsack problem
+#'
+#'The package contains three different functions for solving what is called the knapsack problem.
+#'The knapsack problem is a discrete optimization problem where we have a knapsack that can take a
+#'limited weight 'W' and we want to fill this knapsack with a number of items i = 1, ..., n, each with a
+#'weight w_i and a value v_i. The goal is to find the knapsack with the largest value of the elements added
+#'to the knapsack.
+#'
+#'
+#' @field x
+#' @field W
+#' @import methods
+#' @export knapsack
+#' @exportClass knapsack
+
+knapsack <- setRefClass("knapsack",
+  fields = list(
+    x = "data.frame",
+    W = "numeric",
+    item = "vector"
+  ),
+  methods = list(
+    initialize = function(x, W){
+      check(x,W)
+      x <<- x
+      W <<- W
+      item <<- rep(0,nrow(x))
+    },
+    #check the inputs
+    check = function(a,b){
+      a[a<=0]<-NA
+      if(!is.data.frame(a)||is.na(sum(a))||b<0)
+        stop("Please check your inputs.")
+    },
+    #Brute force search
+    brute_force_knapsack = function(){
+      n <- nrow(x)
+      idx <- 1:(2^n-1)
+      t <- vector()
+      mat <- sapply(idx, function(id){
+        t <- cbind(t, as.integer(intToBits(id)))
+        t
+      })
+      results <- t(x)%*%mat[1:nrow(x),]
+      MAX <- max(as.matrix(results[,results[1,]<=W])[2,])
+      best <- mat[,which(results[2,]==MAX)]
+      list(value=round(MAX,0), elements=which(best==1))
+    },
+    #Dynamic programming
+    knapsack_dynamic = function(){
+      n <- nrow(x)
+      rows <- 1:n
+      cols <- 1:W
+      w <- x[,1]
+      v <- x[,2]
+      mat <- matrix(rep(0,n*W),nrow = n)
+      colnames(mat)<-cols
+      rownames(mat)<-rows
+      for(j in 1:W){
+        for(i in 1:n){
+          if(cols[j]<w[i]){
+            mat[i,j] <- max(mat[i-1,j],0)
+          }else{
+            V_add <- max(mat[i-1,(j-w[i])],0)
+            V_unadd <- max(mat[i-1,j],0)
+            mat[i,j] <- max(V_unadd, V_add + v[i])
+          }
+        }
+      }
+      FindWhat<-function(i, j){
+        if(i>0){
+          if(mat[i,j]==max(mat[i-1,j],0)){
+            item[i]<<-0
+            FindWhat(i-1,j)
+          }
+          else if( j-w[i]>=0 && mat[i,j]==max(mat[i-1,(j-w[i])],0)+v[i]){
+            item[i]<<-1
+            FindWhat(i-1,j-w[i])
+          }
+        }
+      }
+      FindWhat(n,W)
+      list(value=round(max(mat),0), elements=which(item==1))
+    },
+    #Greedy heuristic
+    greedy_knapsack = function(){
+      x1 <- x
+      x1$rows_idx <- row(x1)    # mark the goods with numbers
+      x1 <- x1[x1$w < W,]        # only select the goods who can carry
+      x1$vw_ratio <- x1$v/x1$w   # calculate their price-quality ratios
+      dsc_ordr <- order(x1$vw_ratio, decreasing = TRUE)
+      x1 <- x1[dsc_ordr,]
+
+      rslt <- list(value = 0)
+      curr_weight <- 0
+      i <- 1
+      repeat{
+        if(curr_weight <= W){
+          curr_weight <- curr_weight + x1$w[i]
+          rslt$value <- rslt$value + x1$v[i]
+          rslt$elements[i] <- x1$row[i]
+          i <- i + 1
+          if(i>nrow(x1) || curr_weight+x1$w[i]>W){
+            break()
+          }
+        }
+      }
+      rslt$value <- round(rslt$value,0)
+      return(rslt)
+    }
+  )
+)
